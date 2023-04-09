31743ffa100d4de6536b0bfb71c63d001f547e92
Syed Mansib Ahmed <mansib.syedahmed@gmail.com>
2023-04-08 21:06:02 -0400

    fixed backspace bug

--- Word Auto Completer/GUIDisplay.py
+++ Word Auto Completer/GUIDisplay.py
@@ -1,25 +1,25 @@
-from WordCompleter import tree
+from WordCompleter import tree # import tree from WordCompleter file
 
-import tkinter as tk
+import tkinter as tk # import tkinter GUI library
 
-def find_auto_complete_part(s1:str, s2:str):
+def find_auto_complete_part(s1:str, s2:str): # function to find auto completed part of the word
     return s2.replace(s1, "")
 
 global window
-window = tk.Tk()
+window = tk.Tk() # create a GUI window
 
-window.geometry("300x30")
+window.geometry("300x30") # modifying size of the window
 
-global input_box
+global input_box # create a text box where the user can type
 input_box = tk.Text(window, height=3, font=('Arial', 14), wrap='word', fg="white", insertbackground="white", bg="black")
 
-def auto_completer(event):
+def auto_completer(event): # function to auto complete text
     key_code = 0
     try:
-        key_code = ord(event.keysym)
+        key_code = ord(event.keysym) # find the unicode of the key that was pressed
     except TypeError:
         pass
-    if key_code >= 65 and key_code <= 122:
+    if key_code >= 65 and key_code <= 122 or event.keysym == "BackSpace": # key has to be a letter or backspace
         cursor_index = input_box.index(tk.INSERT) # get index of the cursor
         typed = input_box.get("1.0", cursor_index) # return the text that the user type
         closest_word = tree.find_closest_word(typed) # find closest word
