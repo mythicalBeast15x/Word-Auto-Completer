8f3d1f4901926cc90bb824c67434bd40dc3ad6d8
Syed Mansib Ahmed <mansib.syedahmed@gmail.com>
2023-04-18 16:32:23 -0400

    Revamped the code, added classes and methods, made it look nicer

--- Word Auto Completer/GUIDisplay.py
+++ Word Auto Completer/GUIDisplay.py
@@ -6,81 +6,98 @@ word data and suggesting what the user can
 type from what was input. 
 '''
 
+import tkinter as tk
 from WordCompleter import tree
 
-import tkinter as tk
+# class for the textbox where the user will type
+class Text_box(tk.Text): # inherits from tkinter's Text class
+    def __init_subclass__(cls) -> None: # constructor inherits from super class
+        return super().__init_subclass__()
+    
+    def find_auto_complete_part(self, s1:str, s2:str): # returns part of the word that would be auto completed
+        try:
+            #print('replaced:',s2.replace(s1, ""))
+            return s2.replace(s1, "")
+        except Exception:
+            return " - No suggestions"
+    
+    def recommend_word(self, typed:str): # displays part of the word which will be recommended
+        cursor_index = self.index(tk.INSERT) # get the cursor's index
+        closest_word = tree.find_closest_word(typed) # get the closest word to what the user typed
+        faded_part = self.find_auto_complete_part(typed, closest_word) # recommended portion will be faded
 
-def find_auto_complete_part(s1:str, s2:str): # returns part of the word that would be auto completed
-    try:
-        print('replaced:',s2.replace(s1, ""))
-        return s2.replace(s1, "")
-    except Exception:
-        return " - No suggestions"
+        self.delete(cursor_index, "end") # clear any previous recommendations
 
-global window, frm, input_box, clear_btn
-window = tk.Tk()
-window.title("Word Completer")
+        self.insert(tk.INSERT, faded_part) # insert the faded part after the cursor
+        self.mark_set(tk.INSERT, cursor_index)
 
-window.geometry("300x60")
-# Changes window background to black
-window.configure(bg="black")
+        self.tag_add(faded_part, cursor_index, "end") # create tag for the faded part
+        self.tag_config(faded_part, foreground="gray") # give a gray color
 
-frm = tk.Frame(window)
-# Adds frame to window
-frm.pack()
+    def complete_word(self, event:tk.Event): # function to complete the word when user hits enter key
+        word = self.get(tk.INSERT, "end") # retrieve the word from the text box
+        self.delete(tk.INSERT, "end") # clear the text box
+        self.insert(tk.INSERT, word) # insert the word back with default font color
+        self.mark_set(tk.INSERT, "end-2c")
 
-input_box = tk.Text(frm, height=1, font=('Arial', 14), wrap='word', fg="white", insertbackground="white", bg="black")
+    def get_typed(self) -> str: # return the text that was typed by the user
+        space_index = self.search(" ", "end", backwards=True)
 
-# Function to be called upon button click
-def clear():
-    input_box.delete("1.0", "end")
+        if space_index != "":
+            return self.get(space_index+"+1c", tk.INSERT)
+        else:
+            return self.get("1.0", tk.INSERT)
+        
+    def auto_completer(self, event:tk.Event, window:tk.Tk): # main auto completer function
+        typed = self.get_typed()
 
-# Instantiates button object
-clear_btn = tk.Button(frm, height=1, width=3, text="Clear", fg="white", bg="black",command=clear)
+        if typed == "": # if nothing was typed
+            self.delete(tk.INSERT, "end") # clear any recommendations
+        else:        
+            if self.valid_input(typed):
+                self.recommend_word(typed)
+                window.bind("<Return>", lambda event: self.complete_word(event))
 
-def auto_completer(event):
-    cursor_index = input_box.index(tk.INSERT) # get index of the cursor
-    space_index = input_box.search(" ", "end", backwards=True) # keep track of the space character
+    def valid_input(self, typed:str) -> bool: # validates what the user typed
+        for char in typed:
+            if not char.isalpha(): # check if the character is an alphabet
+                self.delete(tk.INSERT, "end") # clear any recommendations
+                print("Invalid input")
+                return False
+        return True
 
-    if space_index != "": # if space was typed
-        typed = input_box.get(space_index+"+1c", cursor_index) # return the text after the space
-    else:
-        typed = input_box.get("1.0", cursor_index) # return the text that the user type
-
-    key_code = 0
-    try:
-        key_code = ord(event.keysym) # find the unicode of the key
-    except TypeError:
+    def toggle_recommendation(self): # return other recommended words
         pass
 
-    if typed == "": # if nothing was typed
-        input_box.delete(tk.INSERT, "end") # clear any recommendations
-    
-    elif (key_code >= 65 and key_code <= 122) or (event.keysym == "BackSpace" or event.keysym == "space"):
-        #print('typed:',typed, 'reccomended:',tree.find_closest_word(typed))
-        closest_word = tree.find_closest_words(typed,1)[0] # find closest word
-        #closest_word = tree.find_closest_word(typed) # find closest word
-        auto_complete_part = find_auto_complete_part(typed, closest_word) # find the auto-completed part 
+    def clear(self): # clear the textbox
+        self.delete("1.0", "end")
 
-        input_box.delete(cursor_index, "end") # delete everything after the cursor
+class GUI(): # main GUI class encapsulates the window, buttons, textbox
+    def __init__(self) -> None:
+        self.window = tk.Tk() # create window
+        self.frm = tk.Frame(self.window) # create frame
+        # create textbox
+        self.text_box = Text_box(self.frm, height=1, font=('Arial', 14), wrap='word', fg="white", insertbackground="white", bg="black")
+        
+        # Instantiates button object
+        self.clear_btn = tk.Button(self.frm, height=1, width=3, text="Clear", fg="white", bg="black",command=self.text_box.clear)
 
-        input_box.insert(cursor_index, auto_complete_part) # insert the auto completed text after the cursor
-        input_box.mark_set(tk.INSERT, cursor_index)
+    def display(self): # display the GUI
+        self.window.title("Word Completer") # add window title
+        self.window.geometry("300x60") # customize window size/dimensions
+        # Changes window background to black
+        self.window.configure(bg="black")
 
-        input_box.tag_add(auto_complete_part, cursor_index, "end") # create a tag for the autocompleted part
-        input_box.tag_config(auto_complete_part, foreground="gray") # gray it out
+        # Adds frame to window
+        self.frm.pack()
 
-    elif event.keysym == "Return": # if the user hits the ENTER key
-        word = input_box.get(tk.INSERT, "end") # retrieve the word from the text box
-        input_box.delete(tk.INSERT, "end") # clear the text box
-        input_box.insert(tk.INSERT, word) # insert the word back with default font color
-        input_box.mark_set(tk.INSERT, "end-2c")
-    else:
-        print("press a letter key")
+        self.text_box.pack(fill='both', expand=True) # add Text widget to the frame
+        self.clear_btn.pack(fill='both') # Add Button widget to frame
 
-input_box.pack(fill='both', expand=True) # add Text widget to the frame
-clear_btn.pack(fill='both') # Add Button widget to frame
+        # auto completion will execute whenever the user starts typing
+        self.window.bind("<Key>", lambda event: self.text_box.auto_completer(event, self.window))
 
-window.bind("<Key>", auto_completer) # bind the key_pressed function whenever a key is pressed
+        self.window.mainloop()
 
-window.mainloop()
+gui = GUI() # instantiate GUI object
+gui.display() # displays the GUI
